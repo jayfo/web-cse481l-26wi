07bf9171c9793701f9777919ec9c02bd57e68c43
James Fogarty <jayfo@users.noreply.github.com>
2025-12-29 14:35:30 -0800

    Update of Presentations

--- src/data/OfferingData.ts
+++ src/data/OfferingData.ts
@@ -14,9 +14,9 @@ export type OfferingData = {
   linkDrivePresentationSignup: CourseDataLinkHREF;
   // Link to Ed.
   linkEdDiscussion: CourseDataLinkHREF;
-  // Link to course GitLab.
+  // Link to course GitLab, used for student assignments.
   linkGitLab: CourseDataLinkHREF;
-  // Link to course GitHub.
+  // Link to course GitHub, used for this website.
   linkGitHub: CourseDataLinkHREF;
 };
 
@@ -24,37 +24,28 @@ export const offeringData: OfferingData = {
   // Link to Canvas.
   linkCanvas: "https://canvas.uw.edu/courses/1860401",
   // Link to Canvas Paper Presentations.
-  linkCanvasPaperPresentation1DraftSlides: "https://canvas.uw.edu/courses/1798621/assignments/10253217",
-  linkCanvasPaperPresentation1ContributionStatement: "https://canvas.uw.edu/courses/1798621/assignments/10254648",
-  linkCanvasPaperPresentation2DraftSlides: "https://canvas.uw.edu/courses/1798621/assignments/10254647",
-  linkCanvasPaperPresentation2ContributionStatement: "https://canvas.uw.edu/courses/1798621/assignments/10254651",
+  linkCanvasPaperPresentation1DraftSlides: "https://canvas.uw.edu/courses/1860401/assignments/10964899",
+  linkCanvasPaperPresentation1ContributionStatement: "https://canvas.uw.edu/courses/1860401/assignments/10964898",
+  linkCanvasPaperPresentation2DraftSlides: "https://canvas.uw.edu/courses/1860401/assignments/10964901",
+  linkCanvasPaperPresentation2ContributionStatement: "https://canvas.uw.edu/courses/1860401/assignments/10964900",
   // Link to Drive.
-  linkDrive: "https://drive.google.com/drive/folders/1AgjhECjpv4UmvaWFoSCL3sH07oDlcGnj?usp=sharing",
+  linkDrive: "https://drive.google.com/drive/folders/1Ay16rlUBGUL_mCZ6btF0da6jfsCEKWNp?usp=sharing",
   // Link to Drive document for presentation signup.
   linkDrivePresentationSignup: "https://docs.google.com/spreadsheets/d/1dkXhZMsL--e_lmMuVbzRLWUhrDm1vOCLKO3glksqKBo/edit?usp=sharing",
   // Link to Ed.
   linkEdDiscussion: "https://edstem.org/us/courses/77347/discussion",
-  // Link to course GitLab.
-  linkGitLab: "https://gitlab.cs.washington.edu/cse481l-25sp",
-  // Link to course GitHub.
+  // Link to course GitLab, used for student assignments.
+  linkGitLab: "https://gitlab.cs.washington.edu/cse481l-26wi",
+  // Link to course GitHub, used for this website.
   linkGitHub: "https://github.com/uwcse481/web-cse481l-26wi",
 } as const;
 
 // Data from previous offering. To help track what still needs updated.
 //
 // export const offeringData: OfferingData = {
-//     // Link to Canvas Paper Presentations.
-//     linkCanvasPaperPresentation1DraftSlides: "https://canvas.uw.edu/courses/1798621/assignments/10253217",
-//     linkCanvasPaperPresentation1ContributionStatement: "https://canvas.uw.edu/courses/1798621/assignments/10254648",
-//     linkCanvasPaperPresentation2DraftSlides: "https://canvas.uw.edu/courses/1798621/assignments/10254647",
-//     linkCanvasPaperPresentation2ContributionStatement: "https://canvas.uw.edu/courses/1798621/assignments/10254651",
-//     // Link to Drive.
-//     linkDrive: "https://drive.google.com/drive/folders/1AgjhECjpv4UmvaWFoSCL3sH07oDlcGnj?usp=sharing",
 //     // Link to Drive document for presentation signup.
 //     linkDrivePresentationSignup: "https://docs.google.com/spreadsheets/d/1dkXhZMsL--e_lmMuVbzRLWUhrDm1vOCLKO3glksqKBo/edit?usp=sharing",
 //     // Link to Ed.
 //     linkEdDiscussion: "https://edstem.org/us/courses/77347/discussion",
-//     // Link to course GitLab.
-//     linkGitLab: "https://gitlab.cs.washington.edu/cse481l-25sp",
 //   } as const;
   
